74bf990c8e6beee22b6600e7e66778fa6acba2a0
mManuela582279 <manuela.domingues@aluno.senai.br>
2025-07-31 10:34:07 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var numero1 = 2;
+        var numero2 = 3;
+        var soma = numero1 + numero2;
+
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("A soma é igua a:" + soma);
+
+    }
+}
